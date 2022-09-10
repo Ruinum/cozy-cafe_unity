8917e783ca147cd58721f53bad61ef45e3076cb8
RestartYC <neo61483@gmail.com>
2022-09-10 20:16:19 +0300

    cherry-picked from master

--- Cozy-Cafe/Assets/Scripts/TasksSystem/TaskManager.cs
+++ Cozy-Cafe/Assets/Scripts/TasksSystem/TaskManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ruinum.Utils;
+
 
 public class TaskManager : MonoBehaviour
 {
@@ -9,24 +11,51 @@ public class TaskManager : MonoBehaviour
     public List<ScriptableObject> Items = new List<ScriptableObject>();
     [Range(1, 3)] private int TasksNum = 1;
 
+    public float MinChangeTime;
+    public float MaxChangeTime;
+
+
     private void Awake()
     {
         Singletone = this;
         Items.AddRange(Resources.LoadAll<ScriptableObject>("ScriptableObjects/Items"));
     }
 
-    public Task CreateTask(float TimeChangeValue)
+    int hardest; //1-3 repeats
+    public Task CreateTask(Customer customer,float TimeChangeValue)
     {
         Task _task = new Task();
-        _task.Dish.Add(new Dish());
-        _task.Dish[0].Item = AddItem();
+        _task.Owner(customer);
+        hardest = GiveDifficult();
+        for (int i = 0; i < hardest; i++)
+        {
+            _task.Dish.Add(new Dish());
+            _task.Dish[i].Item = AddItem();
+        }
         _task.TskNum = TasksNum;
         TasksNum++;
+        _task.Owner().TimeToWait += Random.Range(MinChangeTime, MinChangeTime);
         return _task;
     }
 
     public ItemSO AddItem()
     {
-        return (ItemSO)Items[0];
+        return (ItemSO)RandomExtentions.RandomList(Items);
+    }
+
+    private int GiveDifficult()
+    {
+        if (RandomExtentions.RandomLess(0.8f))
+        {
+            return 1;
+        }else if (RandomExtentions.RandomLess(0.5f))
+        {
+            return 2;
+        }
+        else
+        {
+            return 3;
+        }
     }
 }
+
