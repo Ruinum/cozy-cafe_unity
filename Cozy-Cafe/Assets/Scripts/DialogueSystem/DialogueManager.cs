49f7e8f3954d981ae7413c00904a62645a964761
slivoviy <s01110011@yandex.ru>
2022-09-13 21:21:43 +0300

    Work in progress

--- Cozy-Cafe/Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Cozy-Cafe/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Articy.Cozy_Cafe;
 using Articy.Unity;
 using Articy.Unity.Interfaces;
 using TMPro;
@@ -10,7 +11,8 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks {
     [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TextMeshProUGUI dialogueText;
     [SerializeField] private TextMeshProUGUI personName;
-    
+    [SerializeField] private RectTransform branchingPanel;
+
     public static DialogueManager Singletone;
     public bool DialogueBoxActive { get; set; }
 
@@ -22,6 +24,7 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks {
     }
 
     public void StartDialogue(IArticyObject aObject) {
+        if (aObject == null) return;
         DialogueBoxActive = true;
         dialogueBox.SetActive(DialogueBoxActive);
 
@@ -35,11 +38,33 @@ public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks {
 
     public void OnFlowPlayerPaused(IFlowObject aObject) {
         dialogueText.text = string.Empty;
+        personName.text = string.Empty;
+
 
         if (aObject is IObjectWithText objectWithText) dialogueText.text = objectWithText.Text;
+
+        if (!(aObject is IObjectWithSpeaker objectWithSpeaker)) return;
+        if (objectWithSpeaker.Speaker is Entity speaker) personName.text = speaker.DisplayName;
     }
 
     public void OnBranchesUpdated(IList<Branch> aBranches) {
-        
+        ClearBranches();
+
+        var dialogueIsFinished = true;
+        foreach (var branch in aBranches) {
+            if (branch.Target is IDialogueFragment) dialogueIsFinished = false;
+        }
+
+        if (dialogueIsFinished) return;
+
+        foreach (var branch in aBranches) {
+            var button = Instantiate(Resources.Load<GameObject>("Prefabs/Dialogue Branch"), branchingPanel);
+        }
+    }
+
+    private void ClearBranches() {
+        foreach (Transform child in branchingPanel) {
+            Destroy(child.gameObject);
+        }
     }
 }
