8e511565bfabb587382326b980ceb8b792783a64
shmyakk <m2008775@edu.misis.ru>
2022-09-14 11:54:56 +0300

    Add MasterMixer

--- Cozy-Cafe/Assets/Scripts/Effects/Parallax.cs
+++ Cozy-Cafe/Assets/Scripts/Effects/Parallax.cs
@@ -7,7 +7,7 @@ public class Parallax : MonoBehaviour, IExecute
     [SerializeField] float offset;
     [SerializeField] float speed;
 
-    private Vector2 _startPos;
+    private Vector3 _startPos;
 
     private void Start()
     {
@@ -22,6 +22,6 @@ public class Parallax : MonoBehaviour, IExecute
         float posX = Mathf.Lerp(transform.position.x, _startPos.x + (mousePos.x * offset), speed * Time.deltaTime);
         float posY = Mathf.Lerp(transform.position.y, _startPos.y + (mousePos.y * offset), speed * Time.deltaTime);
 
-        transform.position = new Vector2(posX, posY);
+        transform.position = new Vector3(posX, posY, _startPos.z);
     }
 }
