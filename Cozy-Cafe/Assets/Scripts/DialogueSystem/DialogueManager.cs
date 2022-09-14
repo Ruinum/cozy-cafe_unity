6c7c141f04d7e8b5095e65352b1c4a20ac3c1fb8
slivoviy <s01110011@yandex.ru>
2022-09-14 23:18:38 +0300

    Finalized feature

--- Cozy-Cafe/Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Cozy-Cafe/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -1,70 +1,73 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using Articy.Cozy_Cafe;
 using Articy.Unity;
 using Articy.Unity.Interfaces;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks {
-    [SerializeField] private GameObject dialogueBox;
-    [SerializeField] private TextMeshProUGUI dialogueText;
-    [SerializeField] private TextMeshProUGUI personName;
-    [SerializeField] private RectTransform branchingPanel;
+namespace DialogueSystem {
+    public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks {
+        [SerializeField] private GameObject dialogueBox;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+        [SerializeField] private TextMeshProUGUI personName;
+        [SerializeField] private RectTransform branchingPanel;
 
-    public static DialogueManager Singletone;
-    public bool DialogueBoxActive { get; set; }
+        // public static DialogueManager Singletone;
 
-    private ArticyFlowPlayer flowPlayer;
+        private ArticyFlowPlayer flowPlayer;
 
-    private void Awake() {
-        Singletone = this;
-        flowPlayer = GetComponent<ArticyFlowPlayer>();
-    }
-
-    public void StartDialogue(IArticyObject aObject) {
-        if (aObject == null) return;
-        DialogueBoxActive = true;
-        dialogueBox.SetActive(DialogueBoxActive);
-
-        flowPlayer.StartOn = aObject;
-    }
-
-    public void CloseDialogueBox() {
-        DialogueBoxActive = false;
-        dialogueBox.SetActive(DialogueBoxActive);
-    }
-
-    public void OnFlowPlayerPaused(IFlowObject aObject) {
-        dialogueText.text = string.Empty;
-        personName.text = string.Empty;
-
-
-        if (aObject is IObjectWithText objectWithText) dialogueText.text = objectWithText.Text;
-
-        if (!(aObject is IObjectWithSpeaker objectWithSpeaker)) return;
-        if (objectWithSpeaker.Speaker is Entity speaker) personName.text = speaker.DisplayName;
-    }
-
-    public void OnBranchesUpdated(IList<Branch> aBranches) {
-        ClearBranches();
-
-        var dialogueIsFinished = true;
-        foreach (var branch in aBranches) {
-            if (branch.Target is IDialogueFragment) dialogueIsFinished = false;
+        private void Awake() {
+            // Singletone = this;
+            flowPlayer = GetComponent<ArticyFlowPlayer>();
         }
 
-        if (dialogueIsFinished) return;
+        public void StartDialogue(IArticyObject aObject) {
+            if (aObject == null) return;
+            dialogueBox.SetActive(true);
 
-        foreach (var branch in aBranches) {
-            var button = Instantiate(Resources.Load<GameObject>("Prefabs/Dialogue Branch"), branchingPanel);
+            flowPlayer.StartOn = aObject;
         }
-    }
 
-    private void ClearBranches() {
-        foreach (Transform child in branchingPanel) {
-            Destroy(child.gameObject);
+        private void CloseDialogueBox() {
+            dialogueBox.SetActive(false);
+        }
+
+        public void OnFlowPlayerPaused(IFlowObject aObject) {
+            dialogueText.text = string.Empty;
+            personName.text = string.Empty;
+
+
+            if (aObject is IObjectWithText objectWithText) dialogueText.text = objectWithText.Text;
+
+            if (!(aObject is IObjectWithSpeaker objectWithSpeaker)) return;
+            if (objectWithSpeaker.Speaker is Entity speaker) personName.text = speaker.DisplayName;
+        }
+
+        public void OnBranchesUpdated(IList<Branch> aBranches) {
+            ClearBranches();
+
+            var dialogueIsFinished = true;
+            foreach (var branch in aBranches) {
+                if (branch.Target is IDialogueFragment) dialogueIsFinished = false;
+            }
+
+            if (!dialogueIsFinished) {
+                foreach (var branch in aBranches) {
+                    var button = Instantiate(Resources.Load<GameObject>("Prefabs/Branch Button"), branchingPanel);
+                    button.GetComponent<BranchSelector>().AssignBranch(flowPlayer, branch);
+                }
+            }
+            else {
+                var button = Instantiate(Resources.Load<GameObject>("Prefabs/Close Button"), branchingPanel);
+                button.GetComponent<Button>().onClick.AddListener(CloseDialogueBox);
+            }
+        }
+
+        private void ClearBranches() {
+            foreach (Transform child in branchingPanel) {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
