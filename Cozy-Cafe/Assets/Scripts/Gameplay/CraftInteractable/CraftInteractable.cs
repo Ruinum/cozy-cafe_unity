2950133e0069ec018ae63955b9f71ea28576cbe5
Illusion <66023494+Mimist-Illusionard@users.noreply.github.com>
2022-09-14 17:15:54 +0300

    Add: - Base items - Finish dishes items - New dish receipts
    Change:
    - CraftInteractable logic

--- Cozy-Cafe/Assets/Scripts/Gameplay/CraftInteractable/CraftInteractable.cs
+++ Cozy-Cafe/Assets/Scripts/Gameplay/CraftInteractable/CraftInteractable.cs
@@ -1,6 +1,8 @@
 using Ruinum.Core;
 using UnityEngine;
+
 using System;
+using System.Collections.Generic;
 
 
 public class CraftInteractable : MonoBehaviour
@@ -8,28 +10,31 @@ public class CraftInteractable : MonoBehaviour
     [SerializeField] private float _craftTime;
     [SerializeField] private ReceptSO[] _recepts;
 
-    private ItemSO _currentItem;
+    private ItemSO _currentCraftedItem;
     private Timer _timer;
+
+    private List<ItemSO> _currentItems = new List<ItemSO>();
 
     private Action<ItemSO> _onCraftComplete;
 
     public bool TryCraftItem(ItemSO itemSO)
     {
-        if (_currentItem != null) return false;
+        if (_currentCraftedItem != null) return false;
         if (_timer != null) return false;
+
+        _currentItems.Add(itemSO);
 
         for (int i = 0; i < _recepts.Length; i++)
         {
-            if (_recepts[i].RequestItem != itemSO) continue;
-
-            var recept = _recepts[i];
-
-            _timer = TimerManager.Singleton.StartTimer(_craftTime, () => { _currentItem = recept.ResultItem; _onCraftComplete?.Invoke(_currentItem); });
-
-            return true;
+            if (_recepts[i].CheckEquality(_currentItems.ToArray())) { _currentItems.Clear(); _onCraftComplete?.Invoke(_recepts[i].ResultItem); return true; }
         }
 
         return false;
+    }
+
+    public void ClearCurrentItems()
+    {
+        _currentItems.Clear();
     }
 
     public void AddListener(Action<ItemSO> listener)
--- Cozy-Cafe/Assets/Scripts/Gameplay/ScriptableObject/ReceptSO.cs
+++ Cozy-Cafe/Assets/Scripts/Gameplay/ScriptableObject/ReceptSO.cs
@@ -4,6 +4,23 @@
 public class ReceptSO : ScriptableObject
 {
     public string Name;
-    public ItemSO RequestItem;
+    public ItemSO[] RequestItems;
     public ItemSO ResultItem;
+
+    public bool CheckEquality(ItemSO[] items)
+    {
+        if (RequestItems.Length > items.Length) return false;
+
+        int correctItems = 0;
+
+        for (int j = 0; j < items.Length; j++)
+        {
+            for (int i = 0; i < RequestItems.Length; i++)
+            {
+                if (items[j] == RequestItems[i]) { correctItems++; break; }
+            }
+        }
+        if (correctItems != RequestItems.Length) return false;
+        return true;
+    }
 }
