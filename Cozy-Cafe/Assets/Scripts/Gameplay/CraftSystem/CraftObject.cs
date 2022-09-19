368623d21a2a7bf48df3a19ac5babff4599f15ea
Mimists-Illusionard <monomonomas1@gmail.com>
2022-09-19 17:43:50 +0300

    Add: - CraftObject UI (in work)

--- Cozy-Cafe/Assets/Scripts/Gameplay/CraftSystem/CraftObject.cs
+++ Cozy-Cafe/Assets/Scripts/Gameplay/CraftSystem/CraftObject.cs
@@ -6,14 +6,14 @@ public class CraftObject : AnimationObject
 {
     private List<ItemSO> _itemSOs = new List<ItemSO>();
     private List<ItemSO> _syrups = new List<ItemSO>();
-    private List<ItemSO> _sprinkles = new List<ItemSO>();
+    private List<ItemSO> _poddings = new List<ItemSO>();
 
     public void AddItem(ItemSO itemSO)
     {
         AnimationPunch();
 
         if (itemSO.IsSyrup) { _syrups.Add(itemSO); return; }
-        if (itemSO.IsTopping) { _sprinkles.Add(itemSO); return; }
+        if (itemSO.IsTopping) { _poddings.Add(itemSO); return; }
 
         _itemSOs.Add(itemSO);
     }
@@ -21,5 +21,21 @@ public class CraftObject : AnimationObject
     private void OnMouseDrag()
     {
         transform.position = new Vector3(MouseUtils.GetMouseWorld2DPosition().x, MouseUtils.GetMouseWorld2DPosition().y, transform.position.z);
+    }
+
+
+    public List<ItemSO> GetItems()
+    {
+        return _itemSOs;
+    }
+
+    public List<ItemSO> GetSyrups()
+    {
+        return _syrups;
+    }
+
+    public List<ItemSO> GetPoddings()
+    {
+        return _poddings;
     }
 }
